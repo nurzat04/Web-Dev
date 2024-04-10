988457d651e13e2dc83736b32347dec1fdee87c5
nurzat04 <123309170+nurzat04@users.noreply.github.com>
2024-04-10 18:12:15 +0500

    Add files via upload

---
+++ hh_back/api/views.py
@@ -0,0 +1,42 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
+
+@api_view(['GET'])
+def company_list(request):
+    companies = Company.objects.all()
+    serializer = CompanySerializer(companies, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def company_detail(request, id):
+    company = get_object_or_404(Company, id=id)
+    serializer = CompanySerializer(company)
+    return JsonResponse(serializer.data)
+
+@api_view(['GET'])
+def company_vacancies(request, id):
+    company = get_object_or_404(Company, id=id)
+    vacancies = company.vacancies.all()
+    serializer = VacancySerializer(vacancies, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all()
+    serializer = VacancySerializer(vacancies, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def vacancy_detail(request, id):
+    vacancy = get_object_or_404(Vacancy, id=id)
+    serializer = VacancySerializer(vacancy)
+    return JsonResponse(serializer.data)
+
+@api_view(['GET'])
+def top_ten_vacancies(request):
+    top_ten = Vacancy.objects.order_by('salary')[:10]
+    serializer = VacancySerializer(top_ten, many=True)
+    return JsonResponse(serializer.data, safe=False)
